4a590e4d1a9e4396048ce0353cb71bc8d2c03915
DreVinciCode <andre.cleaver@tufts.edu>
2022-08-23 13:08:25 -0400

    Update ConvertTwistToTwistCommand.py

--- kortex_driver/src/my_scripts/ConvertTwistToTwistCommand.py
+++ kortex_driver/src/my_scripts/ConvertTwistToTwistCommand.py
@@ -5,18 +5,13 @@ from kortex_driver.msg import TwistCommand
 
 import rospy
 
-
 class messageConverter:
 
     def __init__(self):
     	rospy.init_node('TwistToTwistCommand', anonymous = True)
-
     	self.Twist_sub = rospy.Subscriber("/my_gen3_lite/in/cartesian_velocity_mod", Twist, self.twist_callback)
-
     	self.TwistCommand_pub = rospy.Publisher("/my_gen3_lite/in/cartesian_velocity", TwistCommand, queue_size=1)
-
     	rospy.spin()
-
 
     def twist_callback(self, data):
     	linearX = data.linear.x
@@ -29,7 +24,6 @@ class messageConverter:
     	TwistCommand_Message.twist.linear_z = linearZ
 
     	self.TwistCommand_pub.publish(TwistCommand_Message)
-
 
 if __name__ == '__main__':
     converter = messageConverter()
