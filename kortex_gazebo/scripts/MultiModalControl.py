9a034b23bb468a8f864a4b305b2ea8eeb1d5b737
DreVinciCode <andre.cleaver@tufts.edu>
2023-06-29 13:56:04 -0400

    separated launch files and added models

--- kortex_gazebo/scripts/MultiModalControl.py
+++ kortex_gazebo/scripts/MultiModalControl.py
@@ -4,7 +4,7 @@ import sys
 import time
 import rospy
 from std_srvs.srv import Empty
-from kortex_driver.msg import ActionNotification, ActionEvent
+from kortex_driver.msg import ActionNotification, BaseCyclic_Feedback, ActionEvent, Waypoint, CartesianWaypoint, CartesianReferenceFrame, WaypointList
 from kortex_driver.srv import ReadAction, ReadActionRequest, ExecuteAction, ExecuteActionRequest
 
 class ExampleInitializeGazeboRobot(object):
@@ -41,8 +41,6 @@ class ExampleInitializeGazeboRobot(object):
   def cb_action_topic(self, notif):
     self.last_action_notif_type = notif.action_event
 
-
-
   def wait_for_action_end_or_abort(self):
     while not rospy.is_shutdown():
       if (self.last_action_notif_type == ActionEvent.ACTION_END):
@@ -53,6 +51,61 @@ class ExampleInitializeGazeboRobot(object):
         return False
       else:
         time.sleep(0.01)
+
+  def FillCartesianWaypoint(self, new_x, new_y, new_z, new_theta_x, new_theta_y, new_theta_z, blending_radius):
+    waypoint = Waypoint()
+    cartesianWaypoint = CartesianWaypoint()
+
+    cartesianWaypoint.pose.x = new_x
+    cartesianWaypoint.pose.y = new_y
+    cartesianWaypoint.pose.z = new_z
+    cartesianWaypoint.pose.theta_x = new_theta_x
+    cartesianWaypoint.pose.theta_y = new_theta_y
+    cartesianWaypoint.pose.theta_z = new_theta_z
+    cartesianWaypoint.reference_frame = CartesianReferenceFrame.CARTESIAN_REFERENCE_FRAME_BASE
+    cartesianWaypoint.blending_radius = blending_radius
+    waypoint.oneof_type_of_waypoint.cartesian_waypoint.append(cartesianWaypoint)
+
+    return waypoint
+
+  def lift_arm(self):
+    self.last_action_notif_type = None
+    # Get the actual cartesian pose to increment it
+    # You can create a subscriber to listen to the base_feedback
+    # Here we only need the latest message in the topic though
+    feedback = rospy.wait_for_message("/" + self.robot_name + "/base_feedback", BaseCyclic_Feedback)
+
+    # Possible to execute waypointList via execute_action service or use execute_waypoint_trajectory service directly
+    req = ExecuteActionRequest()
+    trajectory = WaypointList()
+
+    trajectory.waypoints.append(
+        self.FillCartesianWaypoint(
+            feedback.base.commanded_tool_pose_x,
+            feedback.base.commanded_tool_pose_y,
+            feedback.base.commanded_tool_pose_z - 0.20,
+            feedback.base.commanded_tool_pose_theta_x,
+            feedback.base.commanded_tool_pose_theta_y,
+            feedback.base.commanded_tool_pose_theta_z,
+            0)
+    )
+
+    trajectory.duration = 0
+    trajectory.use_optimal_blending = False
+
+    req.input.oneof_action_parameters.execute_waypoint_list.append(trajectory)
+
+    # # Call the service
+    rospy.loginfo("Sending the robot to the cartesian pose...")
+    try:
+        self.execute_action(req)
+    except rospy.ServiceException:
+        rospy.logerr("Failed to call ExecuteWaypointTrajectory")
+        return False
+    else:
+        time.sleep(2)
+        return True
+    
 
   def home_the_robot(self):
     # The Home Action is used to home the robot. It cannot be deleted and is always ID #2:
@@ -104,22 +157,6 @@ class ExampleInitializeGazeboRobot(object):
       else:
         return self.wait_for_action_end_or_abort()
 
-  def FillCartesianWaypoint(self, new_x, new_y, new_z, new_theta_x, new_theta_y, new_theta_z, blending_radius):
-    waypoint = Waypoint()
-    cartesianWaypoint = CartesianWaypoint()
-
-    cartesianWaypoint.pose.x = new_x
-    cartesianWaypoint.pose.y = new_y
-    cartesianWaypoint.pose.z = new_z
-    cartesianWaypoint.pose.theta_x = new_theta_x
-    cartesianWaypoint.pose.theta_y = new_theta_y
-    cartesianWaypoint.pose.theta_z = new_theta_z
-    cartesianWaypoint.reference_frame = CartesianReferenceFrame.CARTESIAN_REFERENCE_FRAME_BASE
-    cartesianWaypoint.blending_radius = blending_radius
-    waypoint.oneof_type_of_waypoint.cartesian_waypoint.append(cartesianWaypoint)
-
-    return waypoint
-
 
 def main():
   try:
@@ -140,7 +177,7 @@ def main():
     success = example.is_init_success
     if success:
       success &= example.home_the_robot()
-      # success &= example.rest_the_robot()
+      success &= example.lift_arm()
 
   except Exception as e:
     print (e)
